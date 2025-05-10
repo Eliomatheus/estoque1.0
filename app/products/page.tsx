1d697d03f62ada277a1e385edd352dee5eef064f
Elio Matheus <eliomatheus8@gmail.com>
2025-05-10 19:30:00 -0300

    chore: add initial tables

---
+++ app/products/page.tsx
@@ -0,0 +1,5 @@
+const ProductsPage = () => {
+  return <h1>Pagina de Produtos</h1>;
+};
+
+export default ProductsPage;
---
+++ app/sales/page.tsx
@@ -0,0 +1,5 @@
+const SalesPage = () => {
+  return <h1>Pagina de Vendas </h1>;
+};
+
+export default SalesPage;
