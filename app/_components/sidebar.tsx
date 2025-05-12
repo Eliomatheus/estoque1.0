de5e8818b3f754af4b21e350932d92e1bea9c921
Elio Matheus <eliomatheus8@gmail.com>
2025-05-11 21:35:44 -0300

    feat: add products tables

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,12 +1,20 @@
 import { LayoutGridIcon, PackageIcon, ShoppingBasketIcon } from "lucide-react";
 import SidebarButton from "./sidebar-button";
+import Image from "next/image";
+import logo from "./image/logo.png"; // Caminho relativo ao 'sidebar.tsx'
 
 const Sidebar = () => {
   return (
     <div className="w-64 bg-white">
       {/* IMAGEM */}
       <div className="px-8 py-6">
-        <h1 className="text-2xl font-bold">Mania Foods</h1>
+        <Image
+          src={logo}
+          alt="Logo Mania Foods"
+          width={120} // Ajuste o tamanho desejado
+          height={48} // Ditado por proporções (LxA)
+          className="object-contain"
+        />
       </div>
       {/* BOTÕES */}
       <div className="flex flex-col gap-2 p-2">
